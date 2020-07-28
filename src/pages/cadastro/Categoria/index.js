8f66b8ae1ca773389c3723ca5bef110273e39e6c
Buzzgrow <artur_pg2011@hotmail.com>
2020-07-28 19:45:23 -0300

    End 28/07

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -6,6 +6,16 @@ function CadastroCategoria(){
     return(
         <PageDefault>
             <h1>Cadastro de Categoria</h1>
+
+                <form>
+                    <label>
+                        Nome da categoria: <input type="text" />
+                    </label>
+                    <button>
+                        Cadastrar
+                    </button>
+                </form>
+
             <Link to="/cadastro/categoria">
                 Hello, there!
             </Link>
